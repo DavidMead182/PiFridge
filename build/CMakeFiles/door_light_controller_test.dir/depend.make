4055d685154f89fe31dfe25f5dc95bcf86b6db95
HamnaSKhalid <hamnakhalid22001@gmail.com>
2026-03-07 10:40:13 -0500

    chore: ignore build outputs and remove tracked build artifacts

--- build/CMakeFiles/door_light_controller_test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for door_light_controller_test.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pifridge/PiFridge/rpi_sensors
-# Build directory: /home/pifridge/PiFridge/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(door_light_controller_test "/home/pifridge/PiFridge/build/door_light_controller_test")
-set_tests_properties(door_light_controller_test PROPERTIES  _BACKTRACE_TRIPLES "/home/pifridge/PiFridge/rpi_sensors/CMakeLists.txt;19;add_test;/home/pifridge/PiFridge/rpi_sensors/CMakeLists.txt;0;")
