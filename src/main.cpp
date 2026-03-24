5240164c7e36d50862507c073c78675d3bb541f1
David Mead <2652016m@student.gla.ac.uk>
2026-03-24 18:55:37 +0000

    rough plan for the main program

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,15 @@ int main() {
 
     // Start up web server
 
-    // Start threads for server vitals
+    // Start threads for server vitals & light sensor & turn on barcode scanner
 
-    // If Door Shut then
-    // Turn barcode scanner off
-    // Fridge vitals uploaded to web app using intervals
-    
-    // If Door Open then
-    // Barcode scanner on, and able to scan and update the web app whenever
-    // Fridge vitals uploaded to web app using intervals
+    // start of infinite loop
+        // If Door Shut then
+        // Turn barcode scanner off
+        // Fridge vitals uploaded to web app using intervals
+        
+        // If Door Open then
+        // Barcode scanner on, and able to scan and update the web app whenever
+        // Fridge vitals uploaded to web app using intervals
     return 0;
 }
