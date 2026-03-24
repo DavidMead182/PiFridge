273ebccd9cf16016d09bb2f2079157e904292b41
David Mead <2652016m@student.gla.ac.uk>
2026-03-24 18:54:35 +0000

    rough plan for the main program

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    std::cout << "PiFridge Starting Up" << std::endl;
+
+    // Start up web server
+
+    // Start threads for server vitals
+
+    // If Door Shut then
+    // Turn barcode scanner off
+    // Fridge vitals uploaded to web app using intervals
+    
+    // If Door Open then
+    // Barcode scanner on, and able to scan and update the web app whenever
+    // Fridge vitals uploaded to web app using intervals
+    return 0;
+}
